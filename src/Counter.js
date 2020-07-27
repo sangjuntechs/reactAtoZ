7a2fd18a8af463a9672201076c02b267b5305999
sangjun <juntech0421@github.com>
2020-07-27 21:31:08 +0900

    useState

--- src/Counter.js
+++ src/Counter.js
@@ -1,22 +1,19 @@
 import React, { useState } from "react";
 
 function Counter() {
-
-  const numberState = useState(0);
-  const number = numberState[0];
-  const setNumber = numberState[1];
+  const [number, setNumber] = useState(0);
 
   const onIncrease = () => {
-    console.log("+1");
+    setNumber(prevNumber => prevNumber + 1);
   };
   const onDecrease = () => {
-    console.log("-1");
+    setNumber(prevNumber => prevNumber - 1);
   };
   return (
     <div>
-      <h1>0</h1>
-      <button onClick={onIncrease}>+1</button>
-      <button onClick={onDecrease}>-1</button>
+      <h1> {number} </h1> 
+      <button onClick={onIncrease}> +1 </button>
+      <button onClick={onDecrease}> -1 </button>
     </div>
   );
 }
